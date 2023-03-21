0e6a8e057ee66a98b795716b82444241f9aab6a5
Klaudia <kklis@interia.pl>
2023-03-21 13:00:04 +0700

    first commit

---
+++ inc/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Scandiweb Assignment</title>
+    <link rel="stylesheet" href="css/style.css"> 
+  </head>
+  
+  <body class="center">
+    
+    <div class="topnav">
+      <a href="#"><h1>Product List</h1></a>
+      <div class="nav-button">
+        <button>ADD</button>
+        <button class="delete-product-btn">MASS DELETE</button>
+      </div>
+    </div>
+    
+    <main>
